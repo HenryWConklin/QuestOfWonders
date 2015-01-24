c179ebefaa7b6b886a9a595f81eb647e36e77d31
tws2xa <tws2xa@virginia.edu>
2015-01-23 20:26:13 -0500

    Map and Player classes

---
+++ QuestOfWonders/QuestOfWonders/Map.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+
+namespace QuestOfWonders
+{
+    class Map
+    {
+
+        //0 = Nothing
+        //1 = Dirt
+        //2 = Grass
+
+        int[,] map;
+
+        public Map(Bitmap img)
+        {
+
+        }
+
+        //Reads in game coords, converts to map coords, and tells you what's there.
+        public int CheckLocation(int x, int y)
+        {
+
+            return 0;
+        }
+    }
+}
