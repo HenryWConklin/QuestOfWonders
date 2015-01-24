6debdd9186a972a63f6fcf38bc2916b47206ca7e
Henry Conklin <henrywconklin@gmail.com>
2015-01-24 13:05:01 -0700

    Animations

---
+++ QuestOfWonders/QuestOfWonders/Animation.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace QuestOfWonders
+{
+    class Animation
+    {
+        private const float FRAME_TIME = 1f / 30;
+
+        private Bitmap[] frames;
+
+        private float time;
+        private int frame;
+
+        public Animation(String name, int numFrames)
+        {
+            frames = new Bitmap[numFrames];
+            for (int i = 0; i < frames.Length; i++)
+            {
+                frames[i] = new Bitmap(Bitmap.FromFile(name + i));
+            }
+
+            frame = 0;
+            time = 0;
+        }
+
+        public void Update(float timePassed)
+        {
+            time += timePassed;
+            while (time > FRAME_TIME)
+            {
+                frame++;
+                time -= FRAME_TIME;
+            }
+        }
+
+        public Bitmap GetFrame()
+        {
+            return frames[frame];
+        }
+
+    
+    }
+}
