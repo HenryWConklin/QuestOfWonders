c5381b17bbfdd301f7b62554cf311b480e0a0c9a
tws2xa <tws2xa@virginia.edu>
2015-01-24 17:19:46 -0500

    .

---
+++ QuestOfWonders/QuestOfWonders/Door.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace QuestOfWonders
+{
+    public class Door
+    {
+        public bool opened;
+        private Point pos;
+        private Bitmap open;
+        private Bitmap closed;
+        
+        private int width;
+        private int height;
+
+        public Door(Point p)
+        {
+            pos = p;
+            opened = false;
+            open = new Bitmap(Bitmap.FromFile("Resources/door open.png"));
+            closed = new Bitmap(Bitmap.FromFile("Resources/door cloosed.png"));
+            width = open.Width;
+            height = open.Height;
+        }
+
+        public void Draw(Graphics g)
+        {
+            g.DrawImage((opened ? open : closed), pos);
+        }
+
+        public Rectangle GetCollisionRectangle()
+        {
+            return (opened?new Rectangle(pos.X, pos.Y, width, height):new Rectangle());
+        }
+    }
+}
--- QuestOfWonders/QuestOfWonders/Form1.cs
+++ QuestOfWonders/QuestOfWonders/Form1.cs
@@ -54,6 +54,8 @@ namespace QuestOfWonders
         public static List<Projectile> projectiles = new List<Projectile>();
         public static List<Enemy> enemies = new List<Enemy>();
         public static Dictionary<Point, PointF> shooters = new Dictionary<Point, PointF>();
+        public static List<Switch> switches = new List<Switch>();
+        public static Door door;
 
         public frmMain()
         {
@@ -159,6 +161,24 @@ namespace QuestOfWonders
                 FireShooters();
                 shooterTimer = 0;
             }
+
+            if (door != null)
+            {
+
+                bool openDoor = true;
+                foreach (Switch s in switches)
+                {
+                    if (!s.isOn)
+                    {
+                        openDoor = false;
+                        break;
+                    }
+                }
+                if (openDoor)
+                {
+                    door = null;
+                }
+            }
             while (timeAccum > FRAME_TIME)
             {
                 if (currentMap != null) currentMap.Update(FRAME_TIME);
@@ -248,13 +268,19 @@ namespace QuestOfWonders
             
             //walls
             Point startPos = new Point(player.GetPos().X, player.GetPos().Y);
+            Rectangle playerRect = player.GetCollisionRectangle();
+            Rectangle doorRect = new Rectangle();
+            if (door != null)
+            {
+                doorRect = door.GetCollisionRectangle();
+            }
 
-            if (CheckPlayerMapCollision())
+            if (CheckPlayerMapCollision() || doorRect.IntersectsWith(playerRect))
             {
                 player.SetPosY((player.GetPos().Y + Map.TILE_SIZE/2) / Map.TILE_SIZE * Map.TILE_SIZE);   
             }               
 
-            if (CheckPlayerMapCollision())
+            if (CheckPlayerMapCollision() || doorRect.IntersectsWith(playerRect))
             {
                 player.SetPosY(startPos.Y);
                 player.SetPosX((player.GetPos().X + Map.TILE_SIZE/2)/ Map.TILE_SIZE * Map.TILE_SIZE);
@@ -264,7 +290,7 @@ namespace QuestOfWonders
                     player.OnCollide(Direction.Left);
             }
 
-            if (CheckPlayerMapCollision())
+            if (CheckPlayerMapCollision() || doorRect.IntersectsWith(playerRect))
             {
                 player.SetPosY((player.GetPos().Y + Map.TILE_SIZE / 2) / Map.TILE_SIZE * Map.TILE_SIZE);
 
@@ -280,7 +306,7 @@ namespace QuestOfWonders
                 player.Ground();
 
             // Enemies
-            Rectangle playerRect = player.GetCollisionRectangle();
+            
             foreach (Enemy e in enemies)
             {
                 if (e.GetCollisionBounds().IntersectsWith(playerRect)) 
@@ -307,6 +333,14 @@ namespace QuestOfWonders
 
             }
 
+            //Switches
+            foreach (Switch s in switches)
+            {
+                if (s.GetCollisionRectangle().IntersectsWith(playerRect))
+                {
+                    s.isOn = true;
+                }
+            }
             
         }
 
