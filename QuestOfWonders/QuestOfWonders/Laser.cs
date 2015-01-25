282b9fd96bf8b31b3553de66468d221ec2a6b18b
tws2xa <tws2xa@virginia.edu>
2015-01-25 00:56:51 -0500

    laser

---
+++ QuestOfWonders/QuestOfWonders/Laser.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace QuestOfWonders
+{
+    public class Laser
+    {
+
+        public int x, y;
+        private int drawWidth = 10*24;
+        private int drawHeight = 9*24;
+        public int width = 5*24;
+        public int height = 9*24;
+        public int widthOffset;
+        Bitmap img;
+        public bool hasLaunched = false;
+
+        Brush firstCol = new SolidBrush(Color.DarkSlateBlue);
+        Brush finalCol = new SolidBrush(Color.DarkSlateGray);
+
+
+        public Laser(int x, int y)
+        {
+            img = new Bitmap(Bitmap.FromFile("Resources/evil laserBig.png"), drawWidth, drawHeight);
+            this.x = x;
+            this.y = y;
+            this.widthOffset = drawWidth - width;
+        }
+
+        public void Draw(Graphics g)
+        {
+            g.DrawImage(img, x - frmMain.viewX, y - frmMain.viewY, drawWidth, drawHeight);
+        }
+
+        public void OnCollide()
+        {
+            if (!hasLaunched)
+            {
+                Textbox text = new Textbox(new List<String>() { "Press Space to use the Laser!", "NO! I leave you for FIVE MINUTES and-" });
+                text.backColor = firstCol;
+                frmMain.text = text;
+                hasLaunched = true;
+                frmMain.allowPlayerControl = false;
+            }
+        }
+
+        public void KeyDown(Keys key)
+        {
+            if (hasLaunched && key == Keys.Space && !frmMain.finalKey)
+            {
+                frmMain.text.backColor = finalCol;
+                frmMain.text.Advance();
+                frmMain.finalKey = true;
+                frmMain.allowPlayerControl = false;
+            }
+        }
+
+        public Rectangle GetCollisionRect()
+        {
+            return new Rectangle(x + widthOffset, y, width, height);
+        }
+
+    }
+}
