467c44c598add783e7491016a58793b3de2e444c
tws2xa <tws2xa@virginia.edu>
2015-01-24 22:37:26 -0500

    d

--- QuestOfWonders/QuestOfWonders/Form1.cs
+++ QuestOfWonders/QuestOfWonders/Form1.cs
@@ -77,7 +77,7 @@ namespace QuestOfWonders
             panelGraphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.NearestNeighbor;
             timeAccum = 0;
 
-            levelMaps = new String[] { "Resources/QuestOfWondersStage4.bmp", "Resources/QuestOfWondersStage2_1.bmp", "Resources/QuestOfWondersStage3.bmp", "Resources/QuestOfWondersStage4.bmp" };
+            levelMaps = new String[] { "Resources/QuestOfWondersStage1.bmp", "Resources/QuestOfWondersStage2_1.bmp", "Resources/QuestOfWondersStage3.bmp", "Resources/QuestOfWondersStage4.bmp" };
             levelGrass = new int[] { 0, 0, 1, 1 };
             currentLevel = 0;
 
