4cd060236d15714c358eefa61df665b0d253601f
Henry Conklin <henrywconklin@gmail.com>
2015-01-25 02:31:21 -0700

    Killed sound effect

--- QuestOfWonders/QuestOfWonders/Form1.cs
+++ QuestOfWonders/QuestOfWonders/Form1.cs
@@ -332,7 +332,7 @@ namespace QuestOfWonders
         public void FireShooters()
         {
             if (shooters.Keys.Count > 0) 
-                SoundSystem.playSound("QuestOfWonders.Resources.Shoot.wav", false);
+                //SoundSystem.playSound("QuestOfWonders.Resources.Shoot.wav", false);
             foreach (Point p in shooters.Keys)
             {
                 Projectile proj = new Projectile(p, shooters[p]);
